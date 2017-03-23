e961eb80495d095cc126dbcfeb6ec6eb004debbd
Adrian Dobrică <adriancnmv@gmail.com>
2017-03-23 14:37:11 +0200

    Revert GUI CraftSystem

---
+++ Assets/Scripts/CraftGUIEnable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CraftGUIEnable : MonoBehaviour {
+    bool enable = false;
+    GameObject craftGUI;
+	// Use this for initialization
+	void Start () {
+        craftGUI = transform.GetChild(0).gameObject;
+        craftGUI.SetActive(enable);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            enable = !enable;
+            craftGUI.SetActive(enable);
+            //Debug.Log("Pressed C!\n");
+        }
+
+    }
+}
