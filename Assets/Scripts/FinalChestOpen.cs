f62a64bdd9ef019dcbd0d9e5d01b2651c29203a7
Adrian Dobrică <adriancnmv@gmail.com>
2017-04-06 23:05:18 +0300

    Add items for Main Island

---
+++ Assets/Scripts/FinalChestOpen.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalChestOpen : MonoBehaviour {
+    public bool nearChest;
+    public Collider2D player;
+
+    // Use this for initialization
+    void Start () {
+        nearChest = false;
+        player = null;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            if (nearChest == true && player != null)
+            {
+ 
+                InventoryNew inventory = player.gameObject.GetComponent<InventoryNew>();
+                if ((inventory.itemExists((int)itemEnum.SPRINGKEY) != -1) &&
+                    (inventory.itemExists((int)itemEnum.SUMMERKEY) != -1) &&
+                    (inventory.itemExists((int)itemEnum.AUTUMNKEY) != -1) &&
+                    (inventory.itemExists((int)itemEnum.WINTERKEY) != -1))
+                {
+                    Debug.Log("Success!");
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            nearChest = true;
+            player = other;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            nearChest = false;
+            player = null;
+        }
+    }
+}
---
+++ Assets/Scripts/WaterCollect.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterCollect : MonoBehaviour {
+    public bool nearWater;
+    public Collider2D player;
+    // Use this for initialization
+    void Start () {
+        nearWater = false;
+        player = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            if (nearWater== true && player != null)
+            {
+                InventoryNew inventory = player.gameObject.GetComponent<InventoryNew>();
+                player.gameObject.GetComponent<InventoryNew>().addItem((int)itemEnum.WATER);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            nearWater = true;
+            player = other;
+            player.gameObject.GetComponent<PlayerStatus>().setStatus(playerPlace.onWater);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            nearWater = false;
+            player.gameObject.GetComponent<PlayerStatus>().setStatus(playerPlace.idle);
+            player = null;
+        }
+    }
+}
