c643bb63d9cc6b7839b81826283a23b8322456dd
Adrian Dobrică <adriancnmv@gmail.com>
2017-04-07 17:22:06 +0300

    Add SignPost and PopUp

---
+++ Assets/Scripts/PopUp.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PopUp : MonoBehaviour
+{
+    public bool nearPopUp;
+    public bool popUpOpen;
+    public Collider2D player;
+    Text text;
+    GameObject newGO;
+    GameObject canvas;
+    // Use this for initialization
+    void Start()
+    {
+        nearPopUp = false;
+        popUpOpen = false;
+        player = null;
+        newGO = new GameObject("myTextGO");
+        text = newGO.AddComponent<Text>();
+        text.text = "Welcome to Plain Island!";
+        Font ArialFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        text.font = ArialFont;
+        text.material = ArialFont.material;
+        newGO.transform.position = new Vector3(0.0f, -100.0f, 0.0f);
+        canvas = GameObject.Find("Canvas");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            if (popUpOpen == false && player != null)
+            {
+                if (nearPopUp == true)
+                {
+                    popUpOpen = true;
+
+                    newGO.transform.SetParent(canvas.transform);
+                    newGO.transform.position = new Vector3(0.0f, -100.0f, 0.0f);
+
+                }
+            }
+
+            else
+            if (popUpOpen == true)
+            {
+                popUpOpen = false;
+                newGO.transform.parent = null;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            nearPopUp = true;
+            player = other;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            nearPopUp = true;
+            player = null;
+        }
+    }
+}
