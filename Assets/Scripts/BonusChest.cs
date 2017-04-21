cb82c2d7f9018e2ca7ae3f07a8f681f1bee4108d
Adrian Dobrică <adriancnmv@gmail.com>
2017-04-21 15:27:57 +0300

    Add kerosene and bonus chest

---
+++ Assets/Scripts/BonusChest.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonusChest : MonoBehaviour {
+	public bool nearChest;
+	public bool isOpen;
+	public Collider2D player;
+	GameObject closedChest;
+	GameObject openedChest;
+	int ticks;
+
+	itemEnum[] reward = {itemEnum.APPLE, itemEnum.IRON, itemEnum.STONE, itemEnum.RAWFISH, itemEnum.PUMPKIN};
+	// Use this for initialization
+	void Start () {
+		nearChest = false;
+		player = null;
+		isOpen = false;
+		closedChest = transform.GetChild (0).gameObject;
+		openedChest = transform.GetChild (1).gameObject;
+		openedChest.SetActive (false);	
+	}
+	
+	void FixedUpdate () {
+		if (Input.GetKeyUp(KeyCode.E))
+		{
+			if (nearChest == true && player != null && isOpen == false)
+			{
+				// open the chest
+				closedChest.SetActive (false);
+				openedChest.SetActive (true);
+				isOpen = true;
+
+				// give reward to the player
+				player.GetComponent<InventoryNew>().addItem((int)reward[Random.Range(0, reward.Length)]);
+			}
+		}
+
+		if (isOpen == true)
+		{
+			ticks++;
+			if (ticks >= 3000)
+			{
+				closedChest.SetActive (true);
+				openedChest.SetActive (false);
+				ticks = 0;
+				isOpen = false;
+			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			nearChest = true;
+			player = other;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			nearChest = false;
+			player = null;
+		}
+	}
+}
--- Assets/Scripts/FinalChestOpen.cs
+++ Assets/Scripts/FinalChestOpen.cs
@@ -3,28 +3,41 @@ using System.Collections;
 
 public class FinalChestOpen : MonoBehaviour {
     public bool nearChest;
+	public bool isOpen;
     public Collider2D player;
+	GameObject closedChest;
+	GameObject openedChest;
 
     // Use this for initialization
     void Start () {
         nearChest = false;
         player = null;
+		isOpen = false;
+		closedChest = transform.GetChild (0).gameObject;
+		openedChest = transform.GetChild (1).gameObject;
+		openedChest.SetActive (false);
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyUp(KeyCode.E))
         {
-            if (nearChest == true && player != null)
+			if (nearChest == true && player != null && isOpen == false)
             {
  
+				// check if the player has got all the keys
                 InventoryNew inventory = player.gameObject.GetComponent<InventoryNew>();
                 if ((inventory.itemExists((int)itemEnum.SPRINGKEY) != -1) &&
                     (inventory.itemExists((int)itemEnum.SUMMERKEY) != -1) &&
                     (inventory.itemExists((int)itemEnum.AUTUMNKEY) != -1) &&
                     (inventory.itemExists((int)itemEnum.WINTERKEY) != -1))
                 {
-                    Debug.Log("Success!");
+					closedChest.SetActive (false);
+					openedChest.SetActive (true);
+					isOpen = true;
+
+					// give kerosene to the player
+					inventory.addItem((int)itemEnum.KEROSENE);
                 }
             }
         }
