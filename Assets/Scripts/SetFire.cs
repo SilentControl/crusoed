13a43a028f34b925eb9ff36728344ccc69c32028
Adrian Dobrică <adriancnmv@gmail.com>
2017-04-29 21:19:11 +0300

    Add Fire SpotLight and Particles and Snow for Winter Island

--- Assets/Scripts/SetFire.cs
+++ Assets/Scripts/SetFire.cs
@@ -6,19 +6,25 @@ public class SetFire : MonoBehaviour {
     public bool nearFireCamp;
     public Collider2D player;
     Animator animator;
+	GameObject light;
+	GameObject particles;
     // Use this for initialization
     void Start () {
         isOnFire = false;
         nearFireCamp = false;
         player = null;
         animator = GetComponent<Animator>();
+		light = transform.GetChild (0).gameObject;
+		light.SetActive (false);
+		particles = transform.GetChild (1).gameObject;
+		particles.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyUp(KeyCode.E))
         {
-            if (nearFireCamp == true && player != null)
+			if (nearFireCamp == true && player != null && isOnFire == false)
             {
                 InventoryNew inventory = player.gameObject.GetComponent<InventoryNew>();
                 int stickPosition = inventory.itemExists((int)itemEnum.STICK);
@@ -31,6 +37,8 @@ public class SetFire : MonoBehaviour {
                     isOnFire = true;
                     animator.SetBool("isOnFire", isOnFire);
 					player.gameObject.GetComponent<PlayerStatus>().setStatus(playerPlace.onLitFirePlace);
+					light.SetActive (true);
+					particles.SetActive (true);
                 }
             }
         }
