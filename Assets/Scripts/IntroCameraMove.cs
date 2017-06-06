8f102b94bed736e568d39dc39e80201480993225
Adrian Dobrică <adriancnmv@gmail.com>
2017-06-06 22:50:49 +0300

    Add Intro Screen Interface

---
+++ Assets/Scripts/IntroCameraMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroCameraMove : MonoBehaviour {
+	float increment = -0.02f;
+	Vector3 newPosition;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		newPosition = transform.position;
+		newPosition.y += increment; 
+		if (newPosition.y < -110.0f) {
+			newPosition.y = 7.3f;
+		}
+		transform.position = newPosition;
+	}
+}
